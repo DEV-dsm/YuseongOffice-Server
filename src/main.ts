60bb5f95ef7996d2482d0b8de3f864e2e76f1cb6
12xii <ur12xii@gmail.com>
2023-09-11 08:32:55 +0900

    init : document & env setting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder } from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+const port = process.env.PORT || 8000;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	await app.listen(process.env.PORT);
+	const config = new DocumentBuilder()
+		.setTitle('YuseongOffice-Server')
+		.setDescription('유성구청과 함께하는 데이터 리빙랩 프로젝트 - 마을주민자치과')
+		.setVersion('0.0.1')
+		.addTag('Yuseong')
+		.build();
+	
+	const doc = SwaggerModule.createDocument(app, config);
+
+	SwaggerModule.setup('/doc', app, doc);
+
+	app.enableCors({
+		origin: '*',
+		methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+	});
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+	);
+
+	await app.listen(port);
 }
 
 bootstrap();
