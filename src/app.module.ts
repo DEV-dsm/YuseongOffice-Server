3cc79d62f45b1dc09322693dca7acf27714f7efa
12xii <ur12xii@gmail.com>
2023-09-11 09:16:36 +0900

    init - database setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,27 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	imports: [],
+	imports: [
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: async (config: ConfigService) => ({
+				type: 'mysql',
+				timezone: 'Asiz/Seoul',
+				port: config.get<number>('DB_PORT'),
+				host: config.get<string>('DB_HOST'),
+				username: config.get<string>('DB_USERNAME'),
+				password: config.get<string>('DB_PASSWORD'),
+				database: config.get<string>('DB_NAME'),
+				entities: [ __dirname + '/**/entity/*.js'],
+				synchronize: false, 
+				logging: false,
+				migrations: [__dirname + '/**/migrations/*.js'],
+				migrationsTableName: 'migrations',
+				autoLoadEntities: true,
+			}),
+			inject: [ConfigService]
+		})
+	],
     controllers: [],
     providers: [],
 })
